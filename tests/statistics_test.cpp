daf2ff338961d180234ecd1304061fc6a24c22d4
Rathan03 <74077198+Rathan03@users.noreply.github.com>
2026-09-25 14:32:03 +0100

    Added additional stats testing

--- tests/statistics_test.cpp
+++ tests/statistics_test.cpp
@@ -2,6 +2,7 @@
 #include "monte_carlo/statistics.h"
 
 #include <cmath>
+#include <stdexcept>
 
 using namespace MonteCarlo;
 
@@ -11,4 +12,22 @@ TEST(Stats_Test, Check_Example)
     EXPECT_NEAR(Statistics::mean(values), 3.0, 1e-5);
     EXPECT_NEAR(Statistics::sample_variance(values), 2.5, 1e-5);
     EXPECT_NEAR(Statistics::std_err(values), std::sqrt(0.5), 1e-5);
+}
+
+TEST(Stats_Test, Mean_Throw)
+{
+    std::span<const double> values{};
+    EXPECT_THROW(Statistics::mean(values), std::invalid_argument);
+}
+
+TEST(Stats_Test, Var_Throw)
+{
+    double values[1] = {1.0};
+    EXPECT_THROW(Statistics::sample_variance(values), std::invalid_argument);
+}
+
+TEST(Stats_Test, Std_Err_Throw)
+{
+    double values[1] = {1.0};
+    EXPECT_THROW(Statistics::std_err(values), std::invalid_argument);
 }
